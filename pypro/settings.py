06a5ae72987a4f882a588da97d2a6a6ac9c7c70c
sirpresident <richardnixon@posteo.org>
2019-06-02 13:44:05 -0300

    desacoplamento de variaveis de ambiente

--- pypro/settings.py
+++ pypro/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'at!&=jn+4wf0wnv4$$d^w@u&o^(it8seb_2w_loxklq26iya57'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool)
